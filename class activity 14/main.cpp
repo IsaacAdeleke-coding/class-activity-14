2eb86cfacf8d16f1d6c9796a937379b4afae31dd
IsaacAdeleke-coding <Isaacadelek3@gmail.com>
2024-09-16 12:10:53 -0700

    Add secret number guessing game with input validation
    - Implemented a C++ program that generates a random secret number between 1 and 100.
    - Continuously prompts the user to guess the number using a while loop.
    - Provides feedback to the user if their guess is too high or too low.
    - Congratulates the user upon correctly guessing the number.
    - Resolved implicit conversion warning by casting time() result to unsigned int in srand().

--- class activity 14/main.cpp
+++ class activity 14/main.cpp
@@ -6,9 +6,34 @@
 //
 
 #include <iostream>
+#include <cstdlib>  // For rand() and srand()
+#include <ctime>    // For time()
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    // Seed the random number generator
+    srand(static_cast<unsigned int>(time(0)));
+
+    // Generate a random secret number between 1 and 100
+    int secretNumber = rand() % 100 + 1;
+    int guess;
+
+    // Ask the user to guess the number
+    cout << "Guess the secret number between 1 and 100: ";
+    cin >> guess;
+
+    // While loop to keep asking until the user guesses correctly
+    while (guess != secretNumber) {
+        if (guess < secretNumber) {
+            cout << "Too low! Try again: ";
+        } else {
+            cout << "Too high! Try again: ";
+        }
+        cin >> guess;
+    }
+
+    // If the user guesses correctly, congratulate them
+    cout << "Congratulations! You guessed the secret number!" << endl;
+
     return 0;
 }
